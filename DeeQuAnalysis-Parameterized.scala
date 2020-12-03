e878beed620e23338079aee21030d39f082c113e
Bandana Das <bandand@amazon.de>
2020-12-03 16:47:35 +0100

    Updated both scala and YAML scripts to accept multiple columns for any deequ metrics and calculate accordingly

--- DeeQuAnalysis-Parameterized.scala
+++ DeeQuAnalysis-Parameterized.scala
@@ -181,59 +181,482 @@ object GlueApp {
         paths
       )
       
-      
-    println("MeanCol  :" + MeanCol)   
-     println("MaximumCol  :" + MaximumCol)   
-      println("MinimumCol  :" + MinimumCol)   
-      
+  // Start of the metrics calculation based on the job parameter.   
+     // Calculate metrics Size()   
    var analysisResult: AnalyzerContext = {
       AnalysisRunner
       // data to run the analysis on
         .onData(dataset)
         // define analyzers that compute metrics
         .addAnalyzer(Size())
-        .addAnalyzer(Completeness(CompletenessCol))  // "partnumber"))
-        .addAnalyzer(ApproxCountDistinct(ApproxCountDistinctCol))  // "partnumber"))
-        .addAnalyzer(Mean(MeanCol))  //"programnumber"))
-        .addAnalyzer(Compliance(ComplianceCol, ComplianceLogic))    // "press", "press = 400"))
-        .addAnalyzer(Correlation(CorrelationCol,CorrelationCol2))  // "incompletechecked", "failurefound"))
-        .addAnalyzer(DataType(DataTypeCol))
-	    .addAnalyzer(CountDistinct(CountDistinctCol))
-		.addAnalyzer(Distinctness(DistinctnessCol))
-		.addAnalyzer(Entropy(EntropyCol))
-		.addAnalyzer(Maximum(MaximumCol))
-		.addAnalyzer(Minimum(MinimumCol))
-		.addAnalyzer(MutualInformation(  Seq(MutualInformationCol, MutualInformationOtheCol) ))
-		//.addAnalyzer(PatternMatch(PatternMatchCol, pattern = PatternMatchPattern))
-		.addAnalyzer(Sum(SumCol))
-		.addAnalyzer(UniqueValueRatio(UniqueValueRatioCol))
-		.addAnalyzer(Uniqueness(UniquenessCol))
+        .run()
+    }
+    
+   val metrics = successMetricsAsDataFrame(spark, analysisResult)
+   
+   
+// Start of computation of other metrics 
+
+ // define and create an empty dataframe for each metrics.
+ 
+  val metricsSchema = StructType(List(
+  StructField("entity", StringType, true),
+  StructField("instance", StringType, true),
+  StructField("name", StringType, true),
+  StructField("value", StringType, true)))
+   var metricsCompleteness = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsApproxCountDistinct = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsMean = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsCompliance = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsCorrelation = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsCountDistinct = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsDataType = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsDistinctness = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsEntropy = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsMaximum = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsMinimum = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsMutualInformation = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsSum = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsUniqueValueRatio = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   var metricsUniqueness = spark.createDataFrame(sc.emptyRDD[Row], metricsSchema)
+   
+  // Start measuring metrics based on the input parameters. 
+  // In case multiple columns are provided to calculate a metrics, split the columns and run the loop accordingly
+  
+  //Calculate Completeness Matrics
+    
+if (CompletenessCol != null || CompletenessCol != ""){	    
+
+    val CompletenessColEach = CompletenessCol.split(",")
+
+	   println("after splitting to a variable anfd foreach :" ) 
+	   CompletenessColEach.foreach(println)
+	   
+	   for (e <- CompletenessColEach) {
+    
+  var analysisResultC: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Completeness(e))
         // compute metrics
         .run()
     }
     
+    val metricsComplt = successMetricsAsDataFrame(spark, analysisResultC)
+    println("metricsC data ::" + metricsComplt.show())
+    metricsCompleteness = metricsCompleteness.unionAll(metricsComplt)
+	   }
+	}
+	
+//Calculate Mean Matrics
 
+	if (MeanCol != null || MeanCol != ""){
+
+    val MeanColEach = MeanCol.split(",")
+
+	   println("after splitting to a variable anfd foreach :" ) 
+	   MeanColEach.foreach(println)
+	   
+	   for (e <- MeanColEach) {
+    
+var analysisResultM: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Mean(e))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsMe = successMetricsAsDataFrame(spark, analysisResultM)
+    println("metricsMe data ::" + metricsMe.show())
+    metricsMean= metricsMean.unionAll(metricsMe)
+	   }
+	}
+	
+
+//Calculate ApproxCountDistinct Matrics
+if (ApproxCountDistinctCol != null || ApproxCountDistinctCol != ""){
+	    
+
+    val ApproxCountDistinctColEach = ApproxCountDistinctCol.split(",")
+
+	   println("after splitting to a variable anfd foreach :" ) 
+	   ApproxCountDistinctColEach.foreach(println)
+	   
+	   for (e <- ApproxCountDistinctColEach) {
+    
+var analysisResultA: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(ApproxCountDistinct(e))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsApproxCnt = successMetricsAsDataFrame(spark, analysisResultA)
+    println("metricsApproxCnt data ::" + metricsApproxCnt.show())
+    metricsApproxCountDistinct = metricsApproxCountDistinct.unionAll(metricsApproxCnt)
+	   }
+	}
+	
+//Calculate CountDistinct Matrics	
+	
+	if (CountDistinctCol != null || CountDistinctCol != ""){
+
+    val CountDistinctColEach = CountDistinctCol.split(",")
+
+	   println("after splitting to a variable anfd foreach :" ) 
+	   CountDistinctColEach.foreach(println)
+	   
+	   for (e <- CountDistinctColEach) {
+    
+var analysisResultCountDist: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(CountDistinct(e))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsCountDist = successMetricsAsDataFrame(spark, analysisResultCountDist)
+    println("metricsCountDist data ::" + metricsCountDist.show())
+    metricsCountDistinct= metricsCountDistinct.unionAll(metricsCountDist)
+	   }
+	}
+	
+//Calculate DataType Matrics	
+
+	if (DataTypeCol != null || DataTypeCol != ""){
+    val DataTypeColEach = DataTypeCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   DataTypeColEach.foreach(println)
+	   
+	   for (e <- DataTypeColEach) {
+    
+var analysisResultDataType: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(DataType(e))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsDataT = successMetricsAsDataFrame(spark, analysisResultDataType)
+    println("metricsDataT data ::" + metricsDataT.show())
+    metricsDataType= metricsDataType.unionAll(metricsDataT)
+	   }
+	}
+	
+//Calculate Distinctness Matrics
+
+	if (DistinctnessCol != null || DistinctnessCol != ""){
+    val DistinctnessColEach = DistinctnessCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   DistinctnessColEach.foreach(println)
+	   
+	   for (e <- DistinctnessColEach) {
+    
+var analysisResultDistinctN: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Distinctness(e))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsDistN = successMetricsAsDataFrame(spark, analysisResultDistinctN)
+    println("metricsDistN data ::" + metricsDistN.show())
+    metricsDistinctness= metricsDistinctness.unionAll(metricsDistN)
+	   }
+	}
+//Calculate Entropy Matrics
+
+	if (EntropyCol != null || EntropyCol != ""){
+	    
+    val EntropyColEach = EntropyCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   EntropyColEach.foreach(println)
+	   
+	   for (e <- EntropyColEach) {
+    
+var analysisResultDistinctEntropy: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Entropy(e))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsEn = successMetricsAsDataFrame(spark, analysisResultDistinctEntropy)
+    println("metricsEn data ::" + metricsEn.show())
+    metricsEntropy= metricsEntropy.unionAll(metricsEn)
+	   }
+	}
+	
+
+//Calculate Maximum Matrics
+
+	if (MaximumCol != null || MaximumCol != ""){
+
+    val MaximumColEach = MaximumCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   MaximumColEach.foreach(println)
+	   
+	   for (e <- MaximumColEach) {
+    
+var analysisResultDistinctMaximum: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Maximum(e))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsMx = successMetricsAsDataFrame(spark, analysisResultDistinctMaximum)
+    println("metricsMx data ::" + metricsMx.show())
+    metricsMaximum= metricsMaximum.unionAll(metricsMx)
+	   }
+	}
+	
+//Calculate Minimum Matrics
+
+	if (MinimumCol != null || MinimumCol != ""){
+
+    val MinimumColEach = MinimumCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   MinimumColEach.foreach(println)
+	   
+	   for (e <- MinimumColEach) {
+    
+var analysisResultDistinctMin: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Minimum(e))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsMin = successMetricsAsDataFrame(spark, analysisResultDistinctMin)
+    println("metricsMin data ::" + metricsMin.show())
+    metricsMinimum= metricsMinimum.unionAll(metricsMin)
+	   }
+	}
+	
+//Calculate Sum Matrics
+
+	if (SumCol != null || SumCol != ""){
+
+    val SumColEach = SumCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   SumColEach.foreach(println)
+	   
+	   for (e <- SumColEach) {
+    
+  var analysisResultSum: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Sum(e ))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsSm= successMetricsAsDataFrame(spark, analysisResultSum)
+    println("metricsSm data ::" + metricsSm.show())
+    metricsSum= metricsSum.unionAll(metricsSm)
+	   }
+	}
+	
+
+//Calculate UniqueValueRatio Matrics
+
+	if (UniqueValueRatioCol != null || UniqueValueRatioCol != ""){
+
+    val UniqueValueRatioColEach = UniqueValueRatioCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   UniqueValueRatioColEach.foreach(println)
+	   
+	   for (e <- UniqueValueRatioColEach) {
+    
+ var analysisUniqR: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(UniqueValueRatio(e ))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsUniqR= successMetricsAsDataFrame(spark, analysisUniqR)
+    println("metricsUniqR data ::" + metricsUniqR.show())
+    metricsUniqueValueRatio= metricsUniqueValueRatio.unionAll(metricsUniqR)
+	   }
+	}
+	
+//Calculate Uniqueness Matrics
+
+	if (UniquenessCol != null || UniquenessCol != ""){
+
+    val UniquenessColEach = UniquenessCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   UniquenessColEach.foreach(println)
+	   
+	   for (e <- UniquenessColEach) {
+    
+var analysisUniqN: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Uniqueness(e ))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsUniqN= successMetricsAsDataFrame(spark, analysisUniqN)
+    println("metricsUniqN data ::" + metricsUniqN.show())
+    metricsUniqueness= metricsUniqueness.unionAll(metricsUniqN)
+	   }
+	}
+	
+//Calculate Compliance Matrics	
+	
+	if (ComplianceCol != null || ComplianceCol != ""){
+
+    val ComplianceColEach = ComplianceCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   ComplianceColEach.foreach(println)
+	   
+    val ComplianceLogicEach = ComplianceLogic.split(",")
+	println("after splitting to a variable and foreach :" ) 
+	   ComplianceLogicEach.foreach(println)
+	   
+	   
+	   for (e <- ComplianceColEach.indices) {
+    
+var analysisResultCompl: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Compliance(ComplianceColEach(e),ComplianceLogicEach(e)))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsCompl = successMetricsAsDataFrame(spark, analysisResultCompl)
+    println("metricsCompl data ::" + metricsCompl.show())
+    metricsCompliance= metricsCompliance.unionAll(metricsCompl)
+	   }
+	}
+	
+//Calculate Correlation Matrics
+
+	if (CorrelationCol != null || CorrelationCol != ""){
+    val CorrelationColEach = CorrelationCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   CorrelationColEach.foreach(println)
+	   
+    val CorrelationCol2Each = CorrelationCol2.split(",")
+	println("after splitting to a variable and foreach :" ) 
+	   CorrelationCol2Each.foreach(println)   
+	   
+	   
+	   for (e <- CorrelationColEach.indices) {
+
+var analysisResultCorr: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(Correlation(CorrelationColEach(e),CorrelationCol2Each(e)))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsCorr = successMetricsAsDataFrame(spark, analysisResultCorr)
+    println("metricsCorr data ::" + metricsCorr.show())
+    metricsCorrelation= metricsCorrelation.unionAll(metricsCorr)
+	   }
+	}
+	
+
+//Calculate MutualInformatio Matrics
+
+	if (MutualInformationCol != null || MutualInformationCol != ""){
+
+    val MutualInformationColEach = MutualInformationCol.split(",")
+
+	   println("after splitting to a variable and foreach :" ) 
+	   MutualInformationColEach.foreach(println)
+	   
+    val MutualInformationOtheColEach = MutualInformationOtheCol.split(",")
+	println("after splitting to a variable and foreach :" ) 
+	   MutualInformationOtheColEach.foreach(println)   
+	   
+	   
+	   for (e <- MutualInformationColEach.indices) {
+    
+var analysisResultMutualI: AnalyzerContext = {
+      AnalysisRunner
+      // data to run the analysis on
+        .onData(dataset)
+        // define analyzers that compute metrics
+        .addAnalyzer(MutualInformation(Seq(MutualInformationColEach(e), MutualInformationOtheColEach(e)) ))
+        // compute metrics
+        .run()
+    }
+    
+    val metricsMutualI = successMetricsAsDataFrame(spark, analysisResultMutualI)
+    println("metricsMutualI data ::" + metricsMutualI.show())
+    metricsMutualInformation= metricsMutualInformation.unionAll(metricsMutualI)
+	   }
+	}
+	
      
-
-// retrieve successfully computed metrics as a Spark data frame
-    var metrics = successMetricsAsDataFrame(spark, analysisResult)
-    
-    println("metrics type ::" + metrics.getClass)
-    
-    println("metrics data ::" + metrics.show())
-    
- 
+ // Create the final dataframe combining all the metrics calculated separately
+	 
+    var metricsFinal = metrics.unionAll(metricsCompleteness).unionAll(metricsApproxCountDistinct).unionAll(metricsMean).unionAll(metricsCompliance).unionAll(metricsCorrelation).unionAll(metricsCountDistinct).unionAll(metricsDataType).unionAll(metricsDistinctness).unionAll(metricsEntropy).unionAll(metricsMaximum).unionAll(metricsMinimum).unionAll(metricsMutualInformation).unionAll(metricsSum).unionAll(metricsUniqueValueRatio).unionAll(metricsUniqueness)
+     println("metricsFinal data ::" + metricsFinal.show())
+     
     
 //Add file name, bucket name and the pk for each row of the metrics obtained and write the data to DynamoDB output table
     
     
-    for (rowMetrics <- metrics.rdd.collect)
+    for (rowMetrics <- metricsFinal.rdd.collect)
     {
         
-    println("rowMetrics ::" + rowMetrics)
-    
-    println("rowMetrics type ::" + rowMetrics.getClass)
-    
+
     //Extract the column values from the 1st row of the metrics and create a dataframe 
         
     var entity = rowMetrics.mkString(",").split(",")(0)
@@ -257,14 +680,17 @@ var schemaForMetric = StructType(List(
      var rddrowMetricsToData = spark.sparkContext.parallelize(rowMetricsToData)
      var metricsDDB = spark.createDataFrame(rddrowMetricsToData, schemaForMetric)
 
+   
+   var bucket_file_name = bucketName + "-" + fileName
+   println("bucket_file_name :" + bucket_file_name)
+   var col_metricName = instance + "-" + name
+   println("col_metricName :" + col_metricName)
     
-    var bucket_file_entity_name = bucketName + "_" + fileName + "_" + entity + "_" + name
-    
-    println("bucket_file_entity_name :" + bucket_file_entity_name)
-    
+
     //add bucket name, file name and a primary key (concatenation of bucket,file,entity and name ) to the dataframe created above
     
-    metricsDDB = metricsDDB.withColumn("bucket_file_entity_name", lit(bucket_file_entity_name))
+    metricsDDB = metricsDDB.withColumn("bucket-file-name", lit(bucket_file_name))
+    metricsDDB = metricsDDB.withColumn("col-metricName", lit(col_metricName))
     metricsDDB = metricsDDB.withColumn("bucket-name", lit(bucketName))
     metricsDDB = metricsDDB.withColumn("file-name", lit(fileName))
     // println("after adding pk and other columns for DDB o/p : " + metricsDDB.show() )
